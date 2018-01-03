b15288eb8566a03f84c839011ab868c4006cc0b4
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2018-01-03 17:36:26 -0300

    fix cluster_test.go

--- mongodb/cluster_test.go
+++ mongodb/cluster_test.go
@@ -9,15 +9,15 @@ import (
 )
 
 func TestClusterService_List(t *testing.T) {
-	_, mux, server := testServer()
+	httpClient, mux, server := testServer()
 	defer server.Close()
 
-	mux.HandleFunc("/groups/123/clusters", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/api/atlas/v1.0/groups/123/clusters", func(w http.ResponseWriter, r *http.Request) {
 		assertMethod(t, "GET", r)
 		fmt.Fprintf(w, `[{"name":"test","mongoDBMajorVersion":"3.4.9"}]`)
 	})
 
-	client := New("test", "password")
+	client := NewClient(httpClient)
 	clusters, _, err := client.Cluster.List("123")
 	expected := []Cluster{Cluster{Name: "test", MongoDBMajorVersion: "3.4.9"}}
 	assert.Nil(t, err)
