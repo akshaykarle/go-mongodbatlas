c59f3a26dec4dc5e8e533119587cc1e5a13e6a9f
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2018-01-03 17:43:17 -0300

    add new fields to the Cluster struct

--- mongodb/cluster.go
+++ mongodb/cluster.go
@@ -25,9 +25,15 @@ type ProviderSettings struct {
 }
 
 type Cluster struct {
+	Id                  string           `json:"id"`
+	GroupId             string           `json:"groupId"`
 	Name                string           `json:"name"`
+	MongoDBVersion      string           `json:"mongoDBVersion"`
+	DiskSizeGB          float64          `json:"diskSizeGB"`
 	MongoDBMajorVersion string           `json:"mongoDBMajorVersion"`
 	BackupEnabled       bool             `json:"backupEnabled"`
+	StateName           string           `json:"stateName"`
+	ReplicationFactor   int              `json:"replicationFactor"`
 	ProviderSettings    ProviderSettings `json:"providerSettings"`
 }
 
