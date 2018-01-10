334d25855d206469e9549a89c217d4f906346628
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2018-01-10 12:49:34 -0300

    fix a bug to use a new instance of cluster when returning the response in create/update

--- mongodb/clusters_test.go
+++ mongodb/clusters_test.go
@@ -14,12 +14,12 @@ func TestClusterService_List(t *testing.T) {
 
 	mux.HandleFunc("/api/atlas/v1.0/groups/123/clusters", func(w http.ResponseWriter, r *http.Request) {
 		assertMethod(t, "GET", r)
-		fmt.Fprintf(w, `{"links":[],"results":[{"name":"test","mongoDBMajorVersion":"3.4.9"}],"totalCount":1}`)
+		fmt.Fprintf(w, `{"links":[],"results":[{"name":"test","mongoDBMajorVersion":"3.4"}],"totalCount":1}`)
 	})
 
 	client := NewClient(httpClient)
 	clusters, _, err := client.Clusters.List("123")
-	expected := []Cluster{Cluster{Name: "test", MongoDBMajorVersion: "3.4.9"}}
+	expected := []Cluster{Cluster{Name: "test", MongoDBMajorVersion: "3.4"}}
 	assert.Nil(t, err)
 	assert.Equal(t, expected, clusters)
 }
@@ -30,12 +30,12 @@ func TestClusterService_Get(t *testing.T) {
 
 	mux.HandleFunc("/api/atlas/v1.0/groups/123/clusters/test", func(w http.ResponseWriter, r *http.Request) {
 		assertMethod(t, "GET", r)
-		fmt.Fprintf(w, `{"name":"test","mongoDBMajorVersion":"3.4.9"}`)
+		fmt.Fprintf(w, `{"name":"test","mongoDBMajorVersion":"3.4"}`)
 	})
 
 	client := NewClient(httpClient)
 	cluster, _, err := client.Clusters.Get("123", "test")
-	expected := &Cluster{Name: "test", MongoDBMajorVersion: "3.4.9"}
+	expected := &Cluster{Name: "test", MongoDBMajorVersion: "3.4"}
 	assert.Nil(t, err)
 	assert.Equal(t, expected, cluster)
 }
@@ -49,11 +49,11 @@ func TestClusterService_Create(t *testing.T) {
 		w.Header().Set("Content-Type", "application/json")
 		expectedBody := map[string]interface{}{
 			"name":                "test",
-			"mongoDBMajorVersion": "3.4.9",
+			"mongoDBMajorVersion": "3.4",
 			"replicationFactor":   float64(3),
-			"diskSizeGB":          0.5,
 			"backupEnabled":       false,
 			"paused":              false,
+			"diskSizeGB":          10.5,
 			"autoScaling": map[string]interface{}{
 				"diskGBEnabled": false,
 			},
@@ -64,22 +64,44 @@ func TestClusterService_Create(t *testing.T) {
 			},
 		}
 		assertReqJSON(t, expectedBody, r)
-		fmt.Fprintf(w, `{"name":"test","mongoDBMajorVersion":"3.4.9"}`)
+		fmt.Fprintf(w, `{
+			"name":"test",
+			"mongoDBMajorVersion":"3.4",
+			"replicationFactor":3,
+			"backupEnabled":false,
+			"diskSizeGB":10,
+			"paused":false,
+			"autoScaling":{
+				"diskGBEnabled":false
+			},
+			"providerSettings":{
+				"providerName":"AWS",
+				"regionName":"US_EAST_1",
+				"instanceSizeName":"M0"
+			}
+		}`)
 	})
 
 	client := NewClient(httpClient)
 	providerSettings := ProviderSettings{ProviderName: "AWS", RegionName: "US_EAST_1", InstanceSizeName: "M0"}
 	params := &Cluster{
 		Name:                "test",
-		MongoDBMajorVersion: "3.4.9",
+		MongoDBMajorVersion: "3.4",
 		ReplicationFactor:   3,
-		DiskSizeGB:          0.5,
 		BackupEnabled:       false,
-		Paused:              false,
+		DiskSizeGB:          10.5,
 		ProviderSettings:    providerSettings,
 	}
 	cluster, _, err := client.Clusters.Create("123", params)
-	expected := params
+	expected := &Cluster{
+		Name:                "test",
+		MongoDBMajorVersion: "3.4",
+		ReplicationFactor:   3,
+		BackupEnabled:       false,
+		Paused:              false,
+		DiskSizeGB:          10,
+		ProviderSettings:    providerSettings,
+	}
 	assert.Nil(t, err)
 	assert.Equal(t, expected, cluster)
 }
@@ -92,38 +114,48 @@ func TestClusterService_Update(t *testing.T) {
 		assertMethod(t, "PATCH", r)
 		w.Header().Set("Content-Type", "application/json")
 		expectedBody := map[string]interface{}{
-			"name":                "test",
-			"mongoDBMajorVersion": "3.4.9",
-			"replicationFactor":   float64(3),
-			"diskSizeGB":          float64(5),
-			"backupEnabled":       false,
-			"paused":              false,
+			"diskSizeGB":    float64(5),
+			"backupEnabled": false,
+			"paused":        false,
 			"autoScaling": map[string]interface{}{
 				"diskGBEnabled": false,
 			},
-			"providerSettings": map[string]interface{}{
-				"providerName":     "AWS",
-				"regionName":       "US_EAST_1",
-				"instanceSizeName": "M0",
-			},
+			"providerSettings": map[string]interface{}{},
 		}
 		assertReqJSON(t, expectedBody, r)
-		fmt.Fprintf(w, `{"name":"test","mongoDBMajorVersion":"3.4.9"}`)
+		fmt.Fprintf(w, `{
+			"name":"test",
+			"mongoDBMajorVersion":"3.4",
+			"replicationFactor":3,
+			"backupEnabled":false,
+			"diskSizeGB":5,
+			"paused":false,
+			"autoScaling":{
+				"diskGBEnabled":false
+			},
+			"providerSettings":{
+				"providerName":"AWS",
+				"regionName":"US_EAST_1",
+				"instanceSizeName":"M0"
+			}
+		}`)
 	})
 
 	client := NewClient(httpClient)
-	providerSettings := ProviderSettings{ProviderName: "AWS", RegionName: "US_EAST_1", InstanceSizeName: "M0"}
 	params := &Cluster{
-		Name:                "test",
-		MongoDBMajorVersion: "3.4.9",
-		ReplicationFactor:   3,
-		DiskSizeGB:          5,
-		BackupEnabled:       false,
-		Paused:              false,
-		ProviderSettings:    providerSettings,
+		DiskSizeGB: float64(5),
 	}
 	cluster, _, err := client.Clusters.Update("123", "test", params)
-	expected := params
+	providerSettings := ProviderSettings{ProviderName: "AWS", RegionName: "US_EAST_1", InstanceSizeName: "M0"}
+	expected := &Cluster{
+		Name:                "test",
+		MongoDBMajorVersion: "3.4",
+		ReplicationFactor:   3,
+		BackupEnabled:       false,
+		Paused:              false,
+		DiskSizeGB:          float64(5),
+		ProviderSettings:    providerSettings,
+	}
 	assert.Nil(t, err)
 	assert.Equal(t, expected, cluster)
 }
