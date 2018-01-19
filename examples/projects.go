ccf687c431900674b234c2377b56cf31e56ba6d0
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2018-01-19 11:17:01 -0300

    add example for the projects api

---
+++ examples/projects.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	dac "github.com/akshaykarle/go-http-digest-auth-client"
+	"github.com/akshaykarle/mongodb-atlas-go/mongodb"
+)
+
+func main() {
+	username := os.Args[1]
+	password := os.Args[2]
+	// orgId := os.Args[3]
+	t := dac.NewTransport(username, password)
+	httpClient := &http.Client{Transport: &t}
+	client := mongodb.NewClient(httpClient)
+
+	// Projects.List example
+	projects, _, err := client.Projects.List()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("projects list: %v\n", projects)
+
+	// Projects.Get example
+	project, _, err := client.Projects.Get(projects[0].ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("project get: %v\n", project)
+
+	// Projects.Create example
+	params := &mongodb.Project{
+		OrgID: orgId,
+		Name:  "test",
+	}
+	project, _, err = client.Projects.Create(params)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("project created: %v\n", project)
+
+	// Projects.GetByName example
+	project, _, err = client.Projects.GetByName("test")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("project get: %v\n", project)
+
+	// Projects.Delete example
+	_, err = client.Projects.Delete(project.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("project deleted: %v\n", project)
+}
