1d56d6a6e1db7a7c74f88342d845ec3f775f2cb0
Brendan Thompson <me@brendanthompson.com>
2019-04-08 12:01:47 +1000

    resolved test issues, added new test
    - Resolved the issue with the failing `TestPrivateIPModeService_Enable` test
    - Added `TestPrivateIPModeService_Disable` test, whilst the test passes I feel like its a false-positive.

--- mongodbatlas/containers_test.go
+++ mongodbatlas/containers_test.go
@@ -246,16 +246,36 @@ func TestPrivateIPModeService_Enable(t *testing.T) {
 	httpClient, mux, server := testServer()
 	defer server.Close()
 
-	mux.HandleFunc("/groups/123/privateIpMode", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/api/atlas/v1.0/groups/123/privateIpMode", func(w http.ResponseWriter, r *http.Request) {
 		assertMethod(t, "PATCH", r)
 		w.Header().Set("Content-Type", "application/json")
-		expectedBody := map[string]interface{}{"enabled": "true"}
+		expectedBody := map[string]interface{}{"enabled": true}
 		assertReqJSON(t, expectedBody, r)
-		fmt.Fprintf(w, `{"enabled": "true"}`)
+		fmt.Fprintf(w, `{"enabled":true}`)
 	})
 
 	client := NewClient(httpClient)
 	resp, err := client.PrivateIPMode.EnablePrivateIPMode("123")
+
+	assert.Nil(t, err)
+	assert.Equal(t, 200, resp.StatusCode)
+}
+
+// While this test passes I think it is incorrect.
+func TestPrivateIPModeService_Disable(t *testing.T) {
+	httpClient, mux, server := testServer()
+	defer server.Close()
+
+	mux.HandleFunc("/api/atlas/v1.0/groups/123/privateIpMode", func(w http.ResponseWriter, r *http.Request) {
+		assertMethod(t, "PATCH", r)
+		w.Header().Set("Content-Type", "application/json")
+		expectedBody := map[string]interface{}{}
+		assertReqJSON(t, expectedBody, r)
+		fmt.Fprintf(w, `{}`)
+	})
+
+	client := NewClient(httpClient)
+	resp, err := client.PrivateIPMode.DisablePrivateIPMode("123")
 
 	assert.Nil(t, err)
 	assert.Equal(t, 200, resp.StatusCode)
