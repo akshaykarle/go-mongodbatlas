10cc97244d68a4771c201297a76ea1f9e1c0bee4
Yu-Wei Huang <hyww1101@gmail.com>
2019-11-05 18:06:24 +0800

    add test for custom rest API base URL
    use response data from Cloud Manager as test case

--- mongodbatlas/root_test.go
+++ mongodbatlas/root_test.go
@@ -26,3 +26,22 @@ func TestRootService_Get(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, expected, root)
 }
+
+func TestCloudManagerRootService_Get(t *testing.T) {
+	httpClient, mux, server := testServer()
+	defer server.Close()
+
+	mux.HandleFunc("/api/public/v1.0/", func(w http.ResponseWriter, r *http.Request) {
+		assertMethod(t, "GET", r)
+		fmt.Fprintf(w, `{"appName":"MongoDB Cloud Manager","build":"2cf9112fd6b8f59c03a9d1d4cbf32004424e728e","links":[{"href":"https://cloud.mongodb.com/api/public/v1.0","rel":"self"},{"href":"https://cloud.mongodb.com/api/public/v1.0/orgs/000000000000000000000000/apiKeys/000000000000000000000000","rel":"http://mms.mongodb.com/apiKeys"},{"href":"https://cloud.mongodb.com/api/public/v1.0/groups","rel":"http://mms.mongodb.com/groups"},{"href":"https://cloud.mongodb.com/api/public/v1.0/admin/backup","rel":"http://mms.mongodb.com/backupAdmin"}],"throttling":false}`)
+	})
+
+	client := NewCustomURLClient(httpClient, "https://cloud.mongodb.com/api/public/v1.0/")
+	root, _, err := client.Root.Get()
+	expected := &Root{
+		AppName: "MongoDB Cloud Manager",
+		Build:   "2cf9112fd6b8f59c03a9d1d4cbf32004424e728e",
+	}
+	assert.Nil(t, err)
+	assert.Equal(t, expected, root)
+}
